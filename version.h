5e914acaa5ab0fa7d85972e9d0f76bbdd67b229d
Lemiort <lemiort@gmail.com>
2015-09-13 02:37:31 +0300

    Added 64-bit targets

--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "22";
-	static const char MONTH[] = "08";
+	static const char DATE[] = "13";
+	static const char MONTH[] = "09";
 	static const char YEAR[] = "2015";
-	static const char UBUNTU_VERSION_STYLE[] =  "15.08";
+	static const char UBUNTU_VERSION_STYLE[] =  "15.09";
 	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
@@ -20,7 +20,7 @@ namespace AutoVersion{
 	static const long REVISION  = 92;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 190;
+	static const long BUILDS_COUNT  = 200;
 	#define RC_FILEVERSION 1,0,6,92
 	#define RC_FILEVERSION_STRING "1, 0, 6, 92\0"
 	static const char FULLVERSION_STRING [] = "1.0.6.92";
