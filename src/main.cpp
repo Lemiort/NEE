ed9d3f39eea09183f1d6a29224799e40f83a783e
Lemiort <lemiort@gmail.com>
2019-01-19 17:35:26 +0300

    Add glew

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,67 @@
+#include <GLFW/glfw3.h>
+#include <chrono>
 #include <iostream>
 #include <memory>
 #include <thread>
-#include <chrono>
-#include <GLFW/glfw3.h>
 
 using namespace std::chrono_literals;
 
 int main(int argc, char const *argv[])
 {
-    std::cout<<"Test";
+    glfwWindowHint(GLFW_SAMPLES, 4);               // 4x antialiasing
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); // We want OpenGL 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,
+                   GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
+    GLFWwindow *window;
+    // glfwSetErrorCallback(ErrorCallback);
     if (!glfwInit())
     {
-        std::cout<<"Cannot init glfw"<<std::endl;
-        return -1;
+        std::cout << "Can not init glfw" << std::endl;
+        exit(EXIT_FAILURE);
     }
-
-    //auto window = std::make_unique<GLFWwindow>(glfwCreateWindow(640, 480, "My Title", NULL, NULL));
-    GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "NEE", NULL, NULL);
     if (!window)
     {
-        // Window or OpenGL context creation failed
-        std::cout<<"Cannot create Opengl context";
+        glfwTerminate();
+        exit(EXIT_FAILURE);
     }
+    // glfwSetFramebufferSizeCallback(window, FrameBufferSizeCallback);
+    // width=WINDOW_WIDTH;
+    // height=WINDOW_HEIGHT;
+    glfwMakeContextCurrent(window);
+    // glfwSetKeyCallback(window, KeyCallback);
+    // glfwSetCursorPosCallback(window,MousePosCallBack);
+    // glfwSetMouseButtonCallback(window, MouseButtonCallback);
+    /*GLenum res = glewInit();
+  if (res != GLEW_OK)
+      {
+          fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+          return 1;
+      }
+  else
+  {
+      printf("\nGLEW status is %d \n", res);
+  }*/
 
-    std::this_thread::sleep_for(10s);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glEnable(GL_CULL_FACE);
+    // glEnable(GL_PROGRAM_POINT_SIZE);
+    glCullFace(GL_FRONT);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+    // //�������� ������������
+    // glEnable(GL_BLEND);
+    // glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+    // glViewport(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
     glfwTerminate();
-    return 0;
+    exit(EXIT_SUCCESS);
 }
