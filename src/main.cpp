828e9768acef91fe622db094c9407e71fce859b0
Lemiort <lemiort@gmail.com>
2019-01-19 16:47:43 +0300

    Add glfw from conan

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <memory>
+#include <thread>
+#include <chrono>
+#include <GLFW/glfw3.h>
+
+using namespace std::chrono_literals;
 
 int main(int argc, char const *argv[])
 {
-    std::cout<<"Hello world!";
+    std::cout<<"Test";
+    if (!glfwInit())
+    {
+        std::cout<<"Cannot init glfw"<<std::endl;
+        return -1;
+    }
+
+    //auto window = std::make_unique<GLFWwindow>(glfwCreateWindow(640, 480, "My Title", NULL, NULL));
+    GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+    if (!window)
+    {
+        // Window or OpenGL context creation failed
+        std::cout<<"Cannot create Opengl context";
+    }
+
+    std::this_thread::sleep_for(10s);
+
+    glfwTerminate();
     return 0;
 }
